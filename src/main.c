a37425402c0830b93e977398f2c8d5d5866ae320
glomdom <22115@uktc-bg.com>
2023-08-31 15:34:11 +0300

    feat: beginning of ast

--- src/main.c
+++ src/main.c
@@ -76,6 +76,7 @@ typedef struct Error {
         ERROR_GENERIC,
         ERROR_SYNTAX,
         ERROR_TODO,
+        ERROR_MAX,
     } type;
 
     char* msg;
@@ -89,6 +90,7 @@ void print_error(Error err) {
     }
 
     printf("error: ");
+    assert(ERROR_MAX == 6);
 
     switch (err.type) {
     case ERROR_TODO:
@@ -160,7 +162,47 @@ Error lex(char* source, char** beg, char** end) {
     return err;
 }
 
-Error parse_expr(char* source) {
+// TODO
+// |-- API to create new Node
+// `-- API to add Node as child
+typedef long long integer_t;
+typedef struct Node {
+    enum NodeType {
+        NODE_TYPE_NONE,
+        NODE_TYPE_INTEGER,
+        NODE_TYPE_PROGRAM,
+        NODE_TYPE_MAX,
+    } type;
+
+    union NodeValue {
+        integer_t integer;
+    } value;
+
+    struct Node** children;
+} Node;
+
+#define nonep(node) ((node).type == NODE_TYPE_NONE)
+#define integerp(node) ((node).type == NODE_TYPE_INTEGER)
+
+// TODO
+// |-- API to create new Binding
+// `-- API to add Binding to Environment
+typedef struct Binding {
+    char* id;
+    Node* value;
+    struct Binding* next;
+} Binding;
+
+// TODO
+// `-- API to create new Environment
+typedef struct Environment {
+    struct Environment* parent;
+    Binding* bind;
+} Environment;
+
+void environment_set() {}
+
+Error parse_expr(char* source, Node* result) {
     char* beg = source;
     char* end = source;
     Error err = ok;
@@ -186,9 +228,10 @@ int main(int argc, char** argv) {
     char* contents = file_contents(path);
 
     if (contents) {
-        Error err = parse_expr(contents);
-        print_error(err);
+        Node expression;
+        Error err = parse_expr(contents, &expression);
 
+        print_error(err);
         free(contents);
     }
 
